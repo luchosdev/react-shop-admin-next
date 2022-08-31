7e58a75554ec995333ccccbf5a470b3cb3b76f81
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-31 16:37:33 -0500

    []

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,14 +1,3 @@
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-];
 import endPoints from '@services/api';
 import useFetch from '@hooks/useFetch';
 import Pagination from '@common/Pagination';
