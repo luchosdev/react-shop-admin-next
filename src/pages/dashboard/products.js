84c964002e29fac0b6d1e828dda8f063ad27995b
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-31 19:46:41 -0500

    [ADD] Alert and useAlert files, with components for work it

--- src/pages/dashboard/products.js
+++ src/pages/dashboard/products.js
@@ -1,18 +1,36 @@
-import { useState, Fragment } from 'react';
+import { useState, useEffect } from 'react';
 import { PlusIcon } from '@heroicons/react/solid';
 import Modal from '@common/Modal';
 import FormProduct from '@components/FormProduct';
+import axios from 'axios';
+import endPoints from '@services/api';
+import useAlert from '@hooks/useAlert';
+import Alert from '@common/Alert';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function product() {
+export default function Products() {
   const [open, setOpen] = useState(false);
   const [products, setProducts] = useState([]);
+  const { alert, setAlert, toggleAlert } = useAlert();
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await axios.get(endPoints.products.allProducts);
+      setProducts(response.data);
+    }
+    try {
+      getProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [alert]);
 
   return (
     <>
+      <Alert alert={alert} handleClose={toggleAlert} />
       <div className="lg:flex lg:items-center lg:justify-between mb-8">
         <div className="min-w-0 flex-1">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
@@ -114,7 +132,7 @@ export default function product() {
         </div>
       </div>
       <Modal open={open} setOpen={setOpen}>
-        <FormProduct />
+        <FormProduct setOpen={setOpen} setAlert={setAlert} />
       </Modal>
     </>
   );
